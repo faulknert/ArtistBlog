ca5ebd93625695fb54e55d877e0df6257753af0d
Trent Faulkner <trent@Trents-MacBook-Pro.local>
2020-11-24 09:36:58 -0500

    pushing new picture and background

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,7 +15,7 @@ const ContactPage = ({
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
       </Helmet>
       <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
+        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/Sample1.jpg')`, marginBottom: 0}}>
           <h1 className="post-title">Recruit an artist</h1>
           <p>Recommend an Artist to join &rarr;</p>
         </div>
